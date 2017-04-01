4c0eea38beaa1a057797968d1ae43d9caeba2de0
Ernie <ernestfrench@gmail.com>
2017-04-01 15:27:08 -0700

    enums

---
+++ replicatedStorage/enums.lua
@@ -0,0 +1,337 @@
+module={}
+
+objects={}
+objects["Brouhahaha"]=90115385
+objects["Korblox"]=375307
+objects["Group"]=918024
+objects['rbadam']=32345429
+objects['TheGamer101']=2231221
+objects['Robloxplorer']=568955350
+objects['Noobangel']=553718984
+objects['NoobangelName']="Noob Attack: Artemis Annhilation"
+objects['Crazybot']=554650579
+objects['CrazybotName']="Crazybot 10000"
+objects['Badges']={MetCreator={name="Met the Creator", assetid=604717013, slug="Creator"}}
+objects['Badges']["GoodDancer"]={name="Very good Dancer", assetid=612449304, slug="Dancer"}
+objects['Badges']["Meme"]={name="Meme lol", assetid=624654288, slug="Meme"}
+objects['Badges']["Noob"]={name="Noob", assetid=629116799, slug="Noob"}
+
+
+objects['Gamepasses']={Fame={name="Fame", assetid=624305945}}
+objects['BrouhahahaTwitter']=3740501293
+
+module["OBJECTS"]=objects
+module['ALLKINDS']={"Studio","Game","Web","Offline","MobileGame","MobileWeb",}
+
+LOCATIONS={MobileWeb=0, MobileGame=1, Web=2, Studio=3, Game=4, Offline=99}
+module["LOCATIONS"]=LOCATIONS
+
+LOCATION2STR={}
+LOCATION2STR[0]="MobileWeb"
+LOCATION2STR[1]="MobileGame"
+LOCATION2STR[2]="Web"
+LOCATION2STR[3]="Studio"
+LOCATION2STR[4]="Game"
+LOCATION2STR[99]="Offline"
+
+module["LOCATION2STR"]=LOCATION2STR
+
+
+
+--find where the userid fits in this range.
+
+--base feb 2016 userid created by month
+module['userid2month']={
+{userid=1, month=0},
+{userid=150, month=1},
+{userid=200, month=2},
+{userid=2100, month=3},
+{userid=2400, month=4},
+{userid=2700, month=5},
+{userid=8100, month=6},
+{userid=2800, month=7},
+{userid=4800, month=8},
+{userid=6900, month=9},
+{userid=9200, month=10},
+{userid=11400, month=11},
+{userid=13700, month=12},
+{userid=16200, month=13},
+{userid=19300, month=14},
+{userid=22700, month=15},
+{userid=27200, month=16},
+{userid=34200, month=17},
+{userid=42000, month=18},
+{userid=54000, month=19},
+{userid=68000, month=20},
+{userid=85000, month=21},
+{userid=109000, month=22},
+{userid=141000, month=23},
+{userid=176000, month=24},
+{userid=216000, month=25},
+{userid=279000, month=26},
+{userid=353000, month=27},
+{userid=461000, month=28},
+{userid=590000, month=29},
+{userid=752000, month=30},
+{userid=1000000, month=31},
+{userid=1200000, month=32},
+{userid=1400000, month=33},
+{userid=1600000, month=34},
+{userid=1900000, month=35},
+{userid=2100000, month=36},
+{userid=2400000, month=37},
+{userid=2700000, month=38},
+{userid=2900000, month=39},
+{userid=3200000, month=40},
+{userid=3600000, month=41},
+{userid=4000000, month=42},
+{userid=4400000, month=43},
+{userid=4700000, month=44},
+{userid=5100000, month=45},
+{userid=5400000, month=46},
+{userid=5900000, month=47},
+{userid=6300000, month=48},
+{userid=6800000, month=49},
+{userid=7300000, month=50},
+{userid=7800000, month=51},
+{userid=8300000, month=52},
+{userid=8800000, month=53},
+{userid=9400000, month=54},
+{userid=10000000, month=55},
+{userid=10500000, month=56},
+{userid=11200000, month=57},
+{userid=13300000, month=58},
+{userid=13900000, month=59},
+{userid=14500000, month=60},
+{userid=15100000, month=61},
+{userid=15700000, month=62},
+{userid=16500000, month=63},
+{userid=17100000, month=64},
+{userid=17800000, month=65},
+{userid=18800000, month=66},
+{userid=19700000, month=67},
+{userid=20500000, month=68},
+{userid=21300000, month=69},
+{userid=22000000, month=70},
+{userid=22800000, month=71},
+{userid=23600000, month=72},
+{userid=24400000, month=73},
+{userid=25300000, month=74},
+{userid=26300000, month=75},
+{userid=28100000, month=76},
+{userid=29300000, month=77},
+{userid=30600000, month=78},
+{userid=31800000, month=79},
+{userid=32800000, month=80},
+{userid=33900000, month=81},
+{userid=35000000, month=82},
+{userid=36300000, month=83},
+{userid=37700000, month=84},
+{userid=39000000, month=85},
+{userid=40500000, month=86},
+{userid=41800000, month=87},
+{userid=43200000, month=88},
+{userid=44800000, month=89},
+{userid=46400000, month=90},
+{userid=47900000, month=91},
+{userid=49400000, month=92},
+{userid=50700000, month=93},
+{userid=52100000, month=94},
+{userid=53500000, month=95},
+{userid=55300000, month=96},
+{userid=56800000, month=97},
+{userid=58500000, month=98},
+{userid=60200000, month=99},
+{userid=61900000, month=100},
+{userid=63900000, month=101},
+{userid=66000000, month=102},
+{userid=68300000, month=103},
+{userid=69900000, month=104},
+{userid=71700000, month=105},
+{userid=73600000, month=106},
+{userid=75500000, month=107},
+{userid=77600000, month=108},
+{userid=79600000, month=109},
+{userid=81800000, month=110},
+{userid=84000000, month=111},
+{userid=86000000, month=112},
+{userid=88100000, month=113},
+{userid=90500000, month=114},
+{userid=93000000, month=115},
+{userid=95200000, month=116},
+{userid=97700000, month=117},
+{userid=100300000, month=118},
+{userid=103500000, month=119},
+{userid=107300000, month=120},
+{userid=112300000, month=121},
+{userid=118100000, month=122},
+{userid=124200000, month=123},
+{userid=130900000, month=124},
+{userid=139300000, month=125},
+{userid=148600000, month=126},
+{userid=157700000, month=127},
+{userid=166900000, month=128},
+{userid=176700000, month=129},
+{userid=188600000, month=130},
+}
+
+assetTypes={}
+assetTypes[1]="Image"
+assetTypes[2]="T-Shirt"
+assetTypes[3]="Audio"
+assetTypes[4]="Mesh"
+assetTypes[5]="Lua"
+assetTypes[6]="HTML"
+assetTypes[7]="Text"
+assetTypes[8]="Hat"
+assetTypes[9]="Place"
+assetTypes[10]="Model"
+assetTypes[11]="Shirt"
+assetTypes[12]="Pants"
+assetTypes[13]="Decal"
+assetTypes[16]="Avatar"
+assetTypes[17]="Head"
+assetTypes[18]="Face"
+assetTypes[19]="Gear"
+assetTypes[21]="Badge"
+assetTypes[22]="Group Emblem"
+assetTypes[24]="Animation"
+assetTypes[25]="Arms"
+assetTypes[26]="Legs"
+assetTypes[27]="Torso"
+assetTypes[28]="Right Arm"
+assetTypes[29]="Left Arm"
+assetTypes[30]="Left Leg"
+assetTypes[31]="Right Leg"
+assetTypes[32]="Package"
+assetTypes[33]="YouTubeVideo"
+assetTypes[34]="Game Pass"
+assetTypes[35]="App"
+assetTypes[37]="Code"
+assetTypes[38]="Plugin"
+assetTypes[39]="SolidModel"
+assetTypes[40]="MeshPart"
+assetTypes[41]="Hair Accessory"
+assetTypes[42]="Face Accessory"
+assetTypes[43]="Neck Accessory"
+assetTypes[44]="Shoulder Accessory"
+assetTypes[45]="Front Accessory"
+assetTypes[46]="Back Accessory"
+assetTypes[47]="Waist Accessory"
+assetTypes[48]="Climb Animation"
+assetTypes[49]="Death Animation"
+assetTypes[50]="Fall Animation"
+assetTypes[51]="Idle Animation"
+assetTypes[52]="Jump Animation"
+assetTypes[53]="Run Animation"
+assetTypes[54]="Swim Animation"
+assetTypes[55]="Walk Animation"
+assetTypes[56]="Pose Animation"
+module["AssetTypes"]=assetTypes
+
+for id,name in pairs(assetTypes) do
+	module[name]=id
+end
+
+local memes={
+	[1]="Blame John",
+	[2]="Builderman is my Dad",
+	[3]="cOld mOld on a sLate pLate",
+	[4]="Soon (TM)",
+	[5]="Daddy Robux",
+	[6]="Check your Robux",
+	[7]="Oof",
+	[8]="John's Laugh",
+	[9]="It's Free",
+	[10]="David Bazooka",
+	[11]="Bacon Hair",
+	[12]="Merely Illuminati",
+}
+
+module['memesoundids']={
+	[8]=130759239,
+	[9]=130770514,
+	[7]=12222242,
+	[10]=12221984,
+	[12]=205299736,
+}
+
+module['memes']=memes
+local lowermemes={}
+for index,meme in ipairs(memes) do
+	lowermemes[index]=string.lower(meme)
+end
+module['lowermemes']=lowermemes
+
+
+function getAssetTypeCounts(avatar)
+	local assetTypeCounts={}
+	for _,asset in pairs(avatar['assets']) do
+		local assetType=assetTypes[asset.assetType.id]
+		if assetTypeCounts[assetType]==nil then
+			assetTypeCounts[assetType]=1
+		else
+			assetTypeCounts[assetType]=assetTypeCounts[assetType]+1
+		end
+	end
+	return assetTypeCounts
+end
+
+module["getAssetTypeCounts"] = getAssetTypeCounts
+
+module['robloxianNamePatterns']={
+	{pattern='^[%u_]+$', name='All Upper'},
+	{pattern='^[%l_]+$', name='All Lower'},
+	{pattern='%d+', name='Has Digits'},
+	{pattern='^%d', name='Starts with Digit'},
+	{pattern='%d%d%d%d%d%d',name='Six Digits'},
+	{pattern='^%d+$',name='All Digits'},
+	{pattern=' ', name='Has Space'},
+	{pattern='111111', name='111111'},
+	{pattern='^the', name='TheGuy', lower=true},
+	{pattern='^.$', name='One Letters'},
+	{pattern='^..$', name='Two Letters'},
+	{pattern='^...$', name='Three Letters'},
+	{pattern='^....$', name='Four Letters'},
+	{pattern='^.....$', name='Five Letters'},
+	{pattern='^[^%d]+%d+[^%d]+$', name='Embedded'},
+	{pattern='^ii.*ii$',name='Magic II',lower=true},
+	{pattern='^xx.*xx$',name='Magic XX',lower=true},
+	{pattern='^zz.*zz$',name='Magic ZZ',lower=true},
+	{pattern='%u%l%u%l%u',name='AlTeRnAtInG'},
+	{pattern='legendary',name='Legend',lower=true},
+	{pattern='monkey',name='Monkey',lower=true},
+	{pattern='[^%d]1$',name='Ends with 1'},
+	{pattern='....................',name='Twenty Chars'}, 
+	{pattern='roblox',name='roblox',lower=true},
+	{pattern='robot',name='robot',lower=true},
+	{pattern='ninja',name='ninja',lower=true},
+	{pattern='game',name='game',lower=true},
+	{pattern='12345',name='12345'},
+	{pattern='_',name='_'},
+	{pattern='sparkle',name='sparkle',lower=true},
+	{pattern='pony',name='pony',lower=true},
+	{pattern='boy',name='boy',lower=true},
+	{pattern='noob',name='noob',lower=true},
+	{pattern='princess',name='princess',lower=true},
+	{pattern='girl',name='girl',lower=true},
+	{pattern='pizza',name='pizza',lower=true},
+	{pattern='poop',name='poop',lower=true},
+	{pattern='Brouhaha',name='Has Brou',lower=true}, 
+}
+
+module['robloxianNameFunctions']={
+	{func=function(name)
+		return string.sub(name,1,1)==string.sub(name,#name,#name)
+	end,name="SameFirstLast"},	
+}
+
+local grey = BrickColor.Gray().Color
+module['blankAvatar']={scales={}, assets={}, playerAvatarType="R6", bodyColors={headColorId=grey, leftArmColorId=grey, leftLegColorId=grey, rightArmColorId=grey, rightLegColorId=grey, torsoColorId=grey}}
+
+module['ServerOwnerKinds']={Automatic=1, ClickFriend=2, SelfClick=3, Group=4, GameInstance=5}
+module['BillboardKinds']={Standard=1, Tiled=2}
+
+module['ToyAssetIds']={501761204,501941216,501941877,501937488,501761631,501942551,501939119,501755654,501942750,
+501941714,501940219,501753915,501941432,501941329,501938652,501938064,501760904,501755345,501761340,}
+return module
--- starterCharacterScripts/repop-if-long-fall.lua
+++ starterCharacterScripts/repop-if-long-fall.lua
@@ -1,12 +1,22 @@
 local falling = false
 local fallst = 0
 
-function freefalling(hit)
+function checkFalling()
 	if isfalling then
 		local gap = tick()-fallst
-		if gap > 1.2 then
+		print("gap:",gap)
+		if gap > 3 then
+			isfalling=false
+			fallst=0
 			repop()
 		end
+		spawn(function() wait(0.2) checkFalling() end)
+	end
+end
+
+function freefalling()
+	if isfalling then
+		checkFalling()
 	else
 		isfalling=true
 		fallst=tick()
@@ -20,11 +30,14 @@ function touched(player)
 	end
 end
 
-local startPos = CFrame.new(Vector3.new(40, 120, 0))
+local startPos = CFrame.new(Vector3.new(129.003, 56.5, -227.003))
 function repop()
+	print("respawning since you fell too long.")
 	local char = script.Parent
 	char:FindFirstChild("HumanoidRootPart").CFrame=startPos
 end
 
 game.Players.LocalPlayer.Character.Humanoid.FreeFalling:connect(freefalling)
+game.Players.LocalPlayer.Character.Humanoid.Jumping:connect(freefalling)
 game.Players.LocalPlayer.Character.Humanoid.Touched:connect(touched)
+game.Players.LocalPlayer.Character.Humanoid.Running:connect(touched)
